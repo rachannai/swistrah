268c6e8b5830a78ddaf41c02f91baa873eab5479
rachannai <ramchanchlani157@gmail.com>
2024-09-19 11:21:30 +0000

    feat: initiated the project

--- contracts/PERC20Sample.sol
+++ contracts/PERC20Sample.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.17;
 import "./PERC20.sol";
 
 contract PERC20Sample is PERC20 {
-    constructor() PERC20("gha", "ahad") {}
+    constructor() PERC20("fdj", "jhf") {}
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
